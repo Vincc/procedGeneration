aec3c92b5a4ab961a405384be0c21e915e9925b2
vinicc <changvincent57@gmail.com>
2020-11-17 23:27:22 +0800

    file

---
+++ generation.py
@@ -0,0 +1,13 @@
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+# from random import randint
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# Axes3D.plot_surface([i for i in range(100)], [i for i in range(100)], [randint(0,10) for i in range(100)])
+# plt.show()
+#
+
+import matplotlib.pyplot as plt
+plt.plot([1,2],[1,2])
+plt.show()
+
