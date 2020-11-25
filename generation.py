268c3c4bf809563824236e212cbb7c66ea81f8be
Vincc <changvincent57@gmail.com>
2020-11-26 01:56:42 +0800

    added circular gradient

--- generation.py
+++ generation.py
@@ -5,16 +5,7 @@ import numpy as np
 import cv2
 import scipy
 
-settings = {"desert": [50, 1, 0.05, 1000, 10], "mount": [150, 6, 0.5, 2, 100], }
-terrain = "mount"
-
 shape = (500, 500)
-scale = settings[terrain][0]
-octaves = settings[terrain][1]  # iterations of detail
-persistence = settings[terrain][2]  # magnitude of distortions (amplitude)
-lacunarity = settings[terrain][3]  # level of detail (freq)
-displace = settings[terrain][4]
-
 worldLayer1 = np.zeros(shape)
 worldLayer2 = np.zeros(shape)
 
@@ -23,27 +14,29 @@ b2 = randint(0,100)
 
 for i in range(shape[0]):
     for j in range(shape[1]):
-        worldLayer1[i][j] = noise.pnoise2(i / 150, j / 150, octaves=4, persistence=0.05,
-                                          lacunarity=3, repeatx=100, repeaty=100, base=b1) * 10
-        worldLayer2[i][j] = noise.pnoise2(i / scale, j / scale, octaves=octaves, persistence=persistence,
-                                          lacunarity=lacunarity, repeatx=1000, repeaty=1000, base=b2) * 100
+        worldLayer1[i][j] = noise.pnoise2(i / 200, j / 200, octaves=3, persistence=0.5,
+                                          lacunarity=2, repeatx=500, repeaty=500, base=b1) * 150
+        worldLayer2[i][j] = noise.pnoise2(i / 100, j / 100, octaves=6, persistence=0.5,
+                                          lacunarity=2, repeatx=500, repeaty=500, base=b2) * 50
 
 
 
+print(1)
 # print(world)
 #
 # cv2.imshow("image", worldLayer1)
 # cv2.imshow("imagee", worldLayer2)
-# cv2.imshow("ima3gee", (np.divide(np.add(worldLayer2,worldLayer1),2)))
+
+
+cv2.imshow("dd", np.divide(np.add(worldLayer2,worldLayer1),2)*arr)
 #
-# cv2.waitKey()
 
 # print(np.divide(np.add(worldLayer2,worldLayer1),2))
 
-mlab.surf(np.divide(np.add(worldLayer2,worldLayer1),2))
+mlab.surf(np.divide(np.add(worldLayer2,worldLayer1),2)*arr)
 mlab.show()
 
-
+cv2.waitKey()
 
 
 
